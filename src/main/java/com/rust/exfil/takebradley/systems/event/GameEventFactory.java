386dc4d5776755426f940f597cc544c8fd9debe0
chris musselman <christopher.musselman04@gmail.com>
2025-11-27 22:21:31 -0600

    fixed error in GameEventFactory

--- src/main/java/com/rust/exfil/takebradley/systems/event/GameEventFactory.java
+++ src/main/java/com/rust/exfil/takebradley/systems/event/GameEventFactory.java
@@ -1,11 +1,11 @@
 package com.rust.exfil.takebradley.systems.event;
 
 import com.rust.exfil.takebradley.model.entity.Player;
+import com.rust.exfil.takebradley.model.entity.Projectile;
 import com.rust.exfil.takebradley.model.entity.interfaces.Entity;
 import com.rust.exfil.takebradley.model.loot.LootItem;
 
 public class GameEventFactory {
-
     public static EntityDeathEvent createEntityDeathEvent(Entity entity) {
         return new EntityDeathEvent(entity);
     }
@@ -18,12 +18,8 @@ public class GameEventFactory {
         return new ExtractionEvent(player);
     }
 
-    public static ProjectileCreatedEvent createProjectileCreatedEvent() {
-        return new ProjectileCreatedEvent();
+    public static ProjectileCreatedEvent createProjectileCreatedEvent(Projectile projectile) {
+        return new ProjectileCreatedEvent(projectile);
     }
 
-    // TODO: Update this method when Projectile class is implemented
-    // public static ProjectileCreatedEvent createProjectileCreatedEvent(Projectile projectile) {
-    //     return new ProjectileCreatedEvent(projectile);
-    // }
 }
